8a4e722c09aa440ce55df2a4151d4e59456f1271
Ilya Radchenko <ilya@burstcreations.com>
2015-02-20 11:52:00 -0500

    Test falsy value

--- test/index.js
+++ test/index.js
@@ -18,7 +18,8 @@ test('Fields and files', function (t) {
 
   request(app)
     .post('/')
-    .field('test[name]', 'Tobi')
+    .field('test[name]', '')
+    .field('test[location]', 'usa')
     .field('test[isUser]', 'true')
     .field('test[age]', '20')
     .field('test[items]', 'backpack,phone,hat')
@@ -30,8 +31,9 @@ test('Fields and files', function (t) {
       t.same(res.body, {
         test: {
           isUser: true,
-          name: 'Tobi',
+          name: '',
           age: 20,
+          location: 'usa',
           items: ['backpack', 'phone', 'hat'],
           image: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA0AAAANCAYAAABy6+R8AAAAAXNSR0IArs4c6QAAAURJREFUKBV9UktqhEAQ9RMRBz879QIJBJcJQcRjzBVm5SW8hKtcYY4hIiFZipBcQN35SUSMmnoyQs8Mk15od9V7VV3vNc9drCiKHpdleeB5/on+H/T/DIIgZ2H8dgjD8M627VdBEPaapk26ritN0/Rt24rzPB+LoggI8wP8SqLDzrKsb1mWJ9d1RSJutTgicHme91VVKWVZSoT9XbPUITJNs/c874wAJgo4jqOgIG6CGI8ZJEl6831fZTsgyS50jOO4G8fxRaBB71VVnf4jgIw8ZoVIAn2eDcNQ2Kq39hAHqqLTe13X/S0gG4easAGdvrqug6xs/mqPPOSHbwKMo8AxTdPpCskEIDtwwK+Sk3GHYRjELMuQYKDc6lOSJBN8gsFIsi9iB79IpT3UhDiY9XR1vIgDjD0jbeXhG2yAqhAJM1++vT94DrZKppaKPgAAAABJRU5ErkJggg=='
         }
